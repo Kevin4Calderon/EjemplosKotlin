5f8a257ec7df44f65c455e16eb2fa84b1a66149e
Kevin Yoan Calderon Garcia <91550461+Kevin4Calderon@users.noreply.github.com>
2024-12-05 12:32:00 -0600

    Update Ejercicio1.kt

--- src/Ejercicio1.kt
+++ src/Ejercicio1.kt
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
     // Solicitar el valor del lado del cuadrado
     print("Introduce el valor del lado del cuadrado: ")
